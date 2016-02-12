f3cf3d22f3468f365fc7f477ecf73881451fc9e2
Mark Johnson <marknotgeorge90@hotmail.com>
2016-02-12 22:21:57 +0000

    Initial Template10 project

---
+++ togglUWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using togglUWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace togglUWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ togglUWP/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+using togglUWP.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace togglUWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Enabled;
+        }
+    }
+}
